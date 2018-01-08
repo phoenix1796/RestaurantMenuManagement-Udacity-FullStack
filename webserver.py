ecac2ad2d5ca42a68827f7d0f1e6e93c0d34db04
Abhishek Chopra <phoenix.prog1796@gmail.com>
2018-01-08 21:14:26 +0530

    Added basic webserver

---
+++ webserver.py
@@ -0,0 +1,31 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class webServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith('/hello'):
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                
+                output = '<html><head><title>Hello</title></head><body>Hello how are you ?</body></html>'
+
+                self.wfile.write(output)
+
+                print(output)
+                return
+        except IOError:
+            self.send_error(404,"File not found%s"%self.path)
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('',port),webServerHandler)
+        print("Server running on %s"%port)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("^C entered")
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
