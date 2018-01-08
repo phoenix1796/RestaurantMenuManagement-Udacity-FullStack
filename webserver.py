72cd4c5adc700aca0203a10db015f5d63400770c
Abhishek Chopra <phoenix.prog1796@gmail.com>
2018-01-08 21:21:06 +0530

    multiple GETS

--- webserver.py
+++ webserver.py
@@ -9,9 +9,16 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 
                 output = '<html><head><title>Hello</title></head><body>Hello how are you ?</body></html>'
-
                 self.wfile.write(output)
-
+                print(output)
+                return
+            if self.path.endswith('/hola'):
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                
+                output = '<html><head><title>Hola</title></head><body>Hola como estas &#161 <a href="/hello">Back to hello</a></body></html>'
+                self.wfile.write(output)
                 print(output)
                 return
         except IOError:
