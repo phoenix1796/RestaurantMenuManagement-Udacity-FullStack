cfd2e015fd64004902beace0a7e450f1b943ad84
Abhishek Chopra <phoenix.prog1796@gmail.com>
2018-01-16 12:24:33 +0530

    Added JSON endpoints

--- database_setup.py
+++ database_setup.py
@@ -24,6 +24,15 @@ class MenuItem(Base):
         Integer, ForeignKey('restaurant.id')
     )
     restaurant = relationship(Restaurant)
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description':self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course
+        }
 
 engine = create_engine(
     'sqlite:///restaurantmenu.db')
--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 '''
 Just another docstring
 '''
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -14,6 +14,19 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def specificMenuItemJSON(restaurant_id,menu_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one() # To check against arbitrary inputs to the API
+    item = session.query(MenuItem).filter_by(id = menu_id,restaurant_id=restaurant_id).one()
+    return jsonify(MenuItem=item.serialize)
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
